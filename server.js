5bb19dd657a846de26beb228ad7452775b592ca6
ravisharma15 <rb.ravi.sharma@gmail.com>
2016-10-02 00:09:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.send('Article one requested and will be seerved here');
+    res.send('Article one requested and will be served here');
+});  
+
+app.get('/article-two', function (req, res) {
+    res.send('Article two requested and will be served here');
 });    
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
